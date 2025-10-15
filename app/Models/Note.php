df17e6a9b242a49331a84e9373c6c22a84e29ae8
Jose Valduz <jose.valduz@unet.edu.ve>
2025-10-15 17:39:02 -0400

    Configuracion de la BD (creacion en Heidi, config .env) Creacion Archivos de migracion (php artisan make:migration create_names_table, php artisan migrate) conociondo el create y sus posibles valores y configuraciones Rollback, uso de refresh (borra todos los batch y vuelve a hacer migrate) reset(borra todos los batch) rollback(tira al batch anterior, o si se define a un punto especifico) migracion de actualizacion (make:migration update_names_table) en esta coloca lo que se quiere modificar en una funcion shema table models se enlazan a la migracion php artisan make:model Name --migration

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
-    // protected $table = "notes";
+    protected $table = "notes";
 
     // protected $fillable = [
     //     "title",
