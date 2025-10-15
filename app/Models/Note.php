221aa98fcba14bac1c47668e8385ead7131bda4a
Jose Valduz <jose.valduz@unet.edu.ve>
2025-10-15 17:36:55 -0400

    Initial commit: Migraciones y Modelos

---
+++ app/Models/Note.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+    // protected $table = "notes";
+
+    // protected $fillable = [
+    //     "title",
+    //     "description",
+    //     "deadline",
+    //     "done"
+    // ]
+    
+    // protected $guarded =[
+
+    // ]
+    
+    // protected $casts = [
+    //     "deadline"=>"date";
+    // ]
+    
+    // protected $hidden = [
+    //     'password'
+    // ]
+
+    // protected $table = "nuevonombre"
+}
